b710af10624eabc289a40d81fc4c82963fdedfec
imGok <imGok@users.noreply.github.com>
2023-09-25 14:52:39 +0000

    deploy: e13e9b605f03c6128b6bfe361e4b400d3ef77e53

--- 659/flutter_service_worker.js
+++ 659/flutter_service_worker.js
@@ -147,11 +147,11 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b
 "splash/img/light-1x.png": "2ea9fd1216889717edb83a0c3f4c2a2f",
 "splash/img/dark-2x.png": "26f05b769dc261a6536fdb257fdb1883",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
-"index.html": "85f71caf5ce6cb4b03a72b8e6ba75a91",
-"/": "85f71caf5ce6cb4b03a72b8e6ba75a91",
+"index.html": "46930bf6a8c24f53b01c1c5b181d74b7",
+"/": "46930bf6a8c24f53b01c1c5b181d74b7",
 "favicon.png": "d6fd96e2d81a9853d4b0870fec11d291",
 "auth.html": "aaf3b82bee208cb193342ff1c7252919",
-"main.dart.js": "70f435aac3b26dfc5e1f604f1a97d45a",
+"main.dart.js": "aad5ccd3962402c5f3f496132d779a9f",
 "manifest.json": "cc4b6aa791018840b65fd0b0e325b201"};
 // The application shell files that are downloaded before a service worker can
 // start.
