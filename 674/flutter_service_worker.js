cb64a2e3e7d7ebde598c946ac025fc44612c1df2
drminh2807 <drminh2807@users.noreply.github.com>
2023-09-25 08:35:26 +0000

    deploy: c1d70698b0539d87db83245a73a2ca1a47feeb00

--- 674/flutter_service_worker.js
+++ 674/flutter_service_worker.js
@@ -147,11 +147,11 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b
 "splash/img/light-1x.png": "2ea9fd1216889717edb83a0c3f4c2a2f",
 "splash/img/dark-2x.png": "26f05b769dc261a6536fdb257fdb1883",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
-"index.html": "c31b85e4120a9fe536e8b009e611275b",
-"/": "c31b85e4120a9fe536e8b009e611275b",
+"index.html": "66b30bad9b04c9018f3598d11f090428",
+"/": "66b30bad9b04c9018f3598d11f090428",
 "favicon.png": "d6fd96e2d81a9853d4b0870fec11d291",
 "auth.html": "aaf3b82bee208cb193342ff1c7252919",
-"main.dart.js": "1765e6742532d549a994c11362d61811",
+"main.dart.js": "49b6317ddfecb716d6b4ed45588334b4",
 "manifest.json": "cc4b6aa791018840b65fd0b0e325b201"};
 // The application shell files that are downloaded before a service worker can
 // start.
