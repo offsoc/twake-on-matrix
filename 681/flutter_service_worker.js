a9733481fefdee66bc64e42ad2179ebcf844e36f
sherlockvn <sherlockvn@users.noreply.github.com>
2023-09-26 16:54:21 +0000

    deploy: d52521ee93e6a510c6446f93c93db7d2d88e9291

--- 681/flutter_service_worker.js
+++ 681/flutter_service_worker.js
@@ -147,11 +147,11 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b
 "splash/img/light-1x.png": "2ea9fd1216889717edb83a0c3f4c2a2f",
 "splash/img/dark-2x.png": "26f05b769dc261a6536fdb257fdb1883",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
-"index.html": "5091a9a6b07cb67f6f0ce6536ebcb93c",
-"/": "5091a9a6b07cb67f6f0ce6536ebcb93c",
+"index.html": "e6ef61add99f35d0a76d25e002abce1c",
+"/": "e6ef61add99f35d0a76d25e002abce1c",
 "favicon.png": "d6fd96e2d81a9853d4b0870fec11d291",
 "auth.html": "aaf3b82bee208cb193342ff1c7252919",
-"main.dart.js": "49b6317ddfecb716d6b4ed45588334b4",
+"main.dart.js": "515884dbeb64cfbb67c8baaf8e0526a0",
 "manifest.json": "cc4b6aa791018840b65fd0b0e325b201"};
 // The application shell files that are downloaded before a service worker can
 // start.
