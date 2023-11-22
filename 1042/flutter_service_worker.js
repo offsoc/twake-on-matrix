76050a5a902889a30c4c865c31e6f40726774b2f
sherlockvn <sherlockvn@users.noreply.github.com>
2023-11-22 04:14:04 +0000

    deploy: f79021b4b864ba12912e6e4c3722955a7dbfef45

--- 1042/flutter_service_worker.js
+++ 1042/flutter_service_worker.js
@@ -7,8 +7,8 @@ const RESOURCES = {"manifest.json": "f3c8e18d38224185c23bbca2dc0e3f4a",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "version.json": "7365ec6445fa30a5f9d906b6d6ccdb55",
 "favicon.png": "f4d7e5e845001780a8c3cfa3ec5e4625",
-"index.html": "1df492c38ba3f0afcaf016221754a2b0",
-"/": "1df492c38ba3f0afcaf016221754a2b0",
+"index.html": "e15a1755ae624883d1d5c97af9bf6daf",
+"/": "e15a1755ae624883d1d5c97af9bf6daf",
 "icons/Icon-192.png": "db01db96eabb3551ac6861c53778900a",
 "icons/Icon-512.png": "7a90a5fbf6971e6f5dbbe91da44cdb97",
 "assets/FontManifest.json": "71a4a82de411f155107da3f8dac64ebd",
@@ -138,7 +138,7 @@ const RESOURCES = {"manifest.json": "f3c8e18d38224185c23bbca2dc0e3f4a",
 "splash/img/light-3x.png": "6603f28f31551efbdf5edbb5f0418c19",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "style.css": "4d1f54fea6fddad3e588e47e3fa65268",
-"main.dart.js": "f70d175a6b8242483dca01c81e23f54d"};
+"main.dart.js": "7889c9e0936000beea30ac0d871cbe47"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
