c3f6468f0c06513568d217df703ad3aaeb65f13b
sherlockvn <sherlockvn@users.noreply.github.com>
2024-01-26 05:17:44 +0000

    deploy: 24fe9108969aedd1980fafd6bf0b59240a11194e

--- 1393/flutter_service_worker.js
+++ 1393/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "b4ac83697fb060ce51b7a94ed3206218",
+const RESOURCES = {"main.dart.js": "a0fb5e67ea51668353db24b64baf4b17",
 "favicon.png": "f4d7e5e845001780a8c3cfa3ec5e4625",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "auth.html": "73a573bd8749ddfade3168f60a98c5ff",
@@ -707,8 +707,8 @@ const RESOURCES = {"main.dart.js": "b4ac83697fb060ce51b7a94ed3206218",
 "canvaskit/skwasm.js": "87063acf45c5e1ab9565dcf06b0c18b8",
 "icons/Icon-512.png": "7a90a5fbf6971e6f5dbbe91da44cdb97",
 "icons/Icon-192.png": "db01db96eabb3551ac6861c53778900a",
-"index.html": "e370bc7ecad5f9a7f5c5e4df74e7aa7a",
-"/": "e370bc7ecad5f9a7f5c5e4df74e7aa7a",
+"index.html": "1061fb605caeb529ad03f3750e4a9f30",
+"/": "1061fb605caeb529ad03f3750e4a9f30",
 "version.json": "f8bb71891509cba9847f3a712a30c510",
 "style.css": "4d1f54fea6fddad3e588e47e3fa65268",
 "manifest.json": "f3c8e18d38224185c23bbca2dc0e3f4a"};
