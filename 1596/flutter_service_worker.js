5cc5c5efa7f3fa97ae951d30b913a1f9f9974a58
Te-Z <Te-Z@users.noreply.github.com>
2024-03-25 15:44:32 +0000

    deploy: ede2c120c44e76d863d74bdbbeda7a714df950eb

--- 1596/flutter_service_worker.js
+++ 1596/flutter_service_worker.js
@@ -20,8 +20,8 @@ const RESOURCES = {"splash/img/light-4x.png": "5b25686015223b6a26cae134abb12117"
 "splash/img/dark-3x.png": "6603f28f31551efbdf5edbb5f0418c19",
 "splash/img/light-1x.png": "22dfe22f0f85ed9cbc3786af33f886a0",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
-"index.html": "7cc1ac2902ece4514bc7795294cfd0b4",
-"/": "7cc1ac2902ece4514bc7795294cfd0b4",
+"index.html": "8a9afb0eed7f96e75c29463b3fe10896",
+"/": "8a9afb0eed7f96e75c29463b3fe10896",
 "canvaskit/canvaskit.js": "eb8797020acdbdf96a12fb0405582c1b",
 "canvaskit/skwasm.js": "87063acf45c5e1ab9565dcf06b0c18b8",
 "canvaskit/skwasm.wasm": "2fc47c0a0c3c7af8542b601634fe9674",
@@ -721,7 +721,7 @@ const RESOURCES = {"splash/img/light-4x.png": "5b25686015223b6a26cae134abb12117"
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "auth.html": "753913b429e0217a2c870bdafd63d063",
 "script.js": "187dde765b2fc2efe647200435c4ddd8",
-"main.dart.js": "061fe51f05a9f16d634b8316d5197533"};
+"main.dart.js": "5b1936b1d1660d90e59613ce7f092375"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
