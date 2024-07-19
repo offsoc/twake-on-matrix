8be5a6209b0524ac2f6873d5d8047e615b030097
Te-Z <Te-Z@users.noreply.github.com>
2024-07-19 08:25:14 +0000

    deploy: 57915139005b61d5e6bacf4893719295212b80f3

--- 1956/flutter_bootstrap.js
+++ 1956/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2367934416"
+    serviceWorkerVersion: "2795416245"
   }
 });
