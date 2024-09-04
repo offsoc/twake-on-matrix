23a6b8adc261da297eed5eaf0c3e304cf6963c06
sherlockvn <sherlockvn@users.noreply.github.com>
2024-09-04 09:03:15 +0000

    deploy: 96e5fc8f5c30fc767a1269813ffc790f36d48707

--- 1992/flutter_bootstrap.js
+++ 1992/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1258691597"
+    serviceWorkerVersion: "2520259378"
   }
 });
