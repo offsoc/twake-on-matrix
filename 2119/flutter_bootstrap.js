febd0d88e88d76564564a07024ef86fc456738b6
KhaledNjim <KhaledNjim@users.noreply.github.com>
2024-11-19 13:45:50 +0000

    deploy: 24c763fdfac6a2cdeeb6366ce22dead0f0e5e28a

--- 2119/flutter_bootstrap.js
+++ 2119/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3212062808"
+    serviceWorkerVersion: "3529240219"
   }
 });
