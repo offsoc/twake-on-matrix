b5b26a726a66ae8b7323f855c065407c5d9a25d6
nqhhdev <nqhhdev@users.noreply.github.com>
2024-11-27 09:47:53 +0000

    deploy: bc6dd7cea3010a71b37d420bc9dd78bd53e0816d

--- 2158/flutter_bootstrap.js
+++ 2158/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "41949998"
+    serviceWorkerVersion: "1995781964"
   }
 });
