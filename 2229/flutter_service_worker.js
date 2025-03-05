141d988d4caf08ee79c7499cb7571a8430b60904
hoangdat <hoangdat@users.noreply.github.com>
2025-03-05 23:03:37 +0000

    deploy: b77f59d642adfd6860f8bffa3622d64988e8ece5

--- 2229/flutter_service_worker.js
+++ 2229/flutter_service_worker.js
@@ -31,8 +31,8 @@ const RESOURCES = {"canvaskit/skwasm.js": "9fa2ffe90a40d062dd2343c7b84caf01",
 "splash/img/branding-4x.png": "be79b42cdc9510f8b72deb26f22af985",
 "splash/img/branding-3x.png": "a8f173d5d7f1f15be6a3f8cb4f00a9bd",
 "style.css": "f70d0365df109f5c620005d49af5d7c0",
-"index.html": "708c9f8884ccd9dd5ad458e0ef0acb78",
-"/": "708c9f8884ccd9dd5ad458e0ef0acb78",
+"index.html": "bbfae2e2c2b3ad257b77c4f91877905b",
+"/": "bbfae2e2c2b3ad257b77c4f91877905b",
 "assets/NOTICES": "0185427a32a46e3599ec3493e6cd5ca2",
 "assets/assets/branding.png": "91040b80ee5fa19af22d2312bf9050d8",
 "assets/assets/info-logo.png": "4405463fea13750f11982c823dca5744",
@@ -737,7 +737,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "9fa2ffe90a40d062dd2343c7b84caf01",
 "manifest.json": "f3c8e18d38224185c23bbca2dc0e3f4a",
 "favicon.png": "2e69080e35d3ca42d9e9f78474df7d20",
 "script.js": "96daaf53d6e724bb2168139b2f515f93",
-"flutter_bootstrap.js": "b053ce828e3bcf24a55651234a09c48e"};
+"flutter_bootstrap.js": "e1f15c692214dc0b12ad3ecf4cc4ce0f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
