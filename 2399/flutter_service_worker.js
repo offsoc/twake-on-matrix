73c0e8d45ced6320cacc95060c258094d565cd39
nqhhdev <nqhhdev@users.noreply.github.com>
2025-06-16 08:05:13 +0000

    deploy: 86d9db038471573f0086aa6a1867c603d9e07136

--- 2399/flutter_service_worker.js
+++ 2399/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"auth.html": "753913b429e0217a2c870bdafd63d063",
 "splash/img/branding-dark-1x.png": "2063bc3bb018dc3679fa741c6111ae3e",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "manifest.json": "f3c8e18d38224185c23bbca2dc0e3f4a",
-"main.dart.js": "45ca20c3c8f45218ea5c956ba9c529e4",
+"main.dart.js": "729452e2fb3c0bd45c68356882504352",
 "style.css": "f70d0365df109f5c620005d49af5d7c0",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "index.html": "432d30e09df1f4fc87abcfdb83027a2a",
@@ -739,7 +739,7 @@ const RESOURCES = {"auth.html": "753913b429e0217a2c870bdafd63d063",
 "icons/Icon-512.png": "8cb72d8ec7a7be72007869f008b36bfa",
 "icons/Icon-192.png": "c4a581a01d1e71f9a111bdb9daace08d",
 "script.js": "96daaf53d6e724bb2168139b2f515f93",
-"flutter_bootstrap.js": "a5f7f27902e15528cccacbe92477fda1",
+"flutter_bootstrap.js": "969e99f88888473d379618ea7c2a000d",
 "version.json": "e6acd31fd7f756c03bd90e4bb71de6cb"};
 // The application shell files that are downloaded before a service worker can
 // start.
